91c35f66470d93406276399a90bfcfa4f5ead662
IvanSolis2003 <ivan.solis20.m@gmail.com>
2026-07-15 23:47:58 -0400

    fix: polyfill Array.prototype.toReversed in metro config for older Node in EAS build

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,3 +1,7 @@
+if (!Array.prototype.toReversed) {
+  Array.prototype.toReversed = function () { return [...this].reverse(); };
+}
+
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
